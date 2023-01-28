658e58ad94d9de13f9a4fcb9c0b56078c7c9d665
KSZ08M <KSZ08M@cyber-dojo.org>
2023-01-28 14:46:02 +0000

    8 ran tests, no prediction, got amber

--- files/Hiker.kt
+++ files/Hiker.kt
@@ -3,7 +3,7 @@ package hiker
 fun answer():Int {
     val initialState = Array(100){0}
     initialState.forEachIndexed{index, i ->{
-        if(it % 2 == 0){
+        if(i % 2 == 0){
                 initialState[index] = 1
         }
         print(i)
