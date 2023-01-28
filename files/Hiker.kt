f41d542fd16c2a5c25ce843abf5314e2781fb8b1
KSZ08M <KSZ08M@cyber-dojo.org>
2023-01-28 15:06:57 +0000

    29 ran tests, no prediction, got green

--- files/Hiker.kt
+++ files/Hiker.kt
@@ -5,13 +5,13 @@ import java.util.Arrays
 fun answer():Int {
     val initialState = Array(100){true}
     for (i in 1..100){
-        print(i)
-    }
-    initialState.forEachIndexed{ index , i ->
+        initialState.forEachIndexed{ index , i ->
         if(index % 2 == 1){
             initialState[index] = !initialState[index]
         }
     }
+    }
+    
     
     println(Arrays.toString(initialState))
     return initialState.size
