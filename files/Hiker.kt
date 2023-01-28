b05f29ddadbebf94ea15eb78248ea8add6f057bd
KSZ08M <KSZ08M@cyber-dojo.org>
2023-01-28 15:22:20 +0000

    37 ran tests, no prediction, got green

--- files/Hiker.kt
+++ files/Hiker.kt
@@ -6,17 +6,17 @@ fun answer():Int {
     val initialState = Array(100){true}
     
     for (i in 1..100){
+        /*
         if(i == 1){
             initialState.forEachIndexed{ index , i ->
                                         if(index!== 0 && index % 2 == 0){
                                             initialState[index-1] = !initialState[index-1]
                                         }
                                        }
-        }
+        }*/
         if(i == 2){
             initialState.forEachIndexed{ index , i ->
                                         if(index!== 0 && index % 3 == 1){
-                                            print(index)
                                             initialState[index-1] = !initialState[index-1]
                                         }
                                        }
