dd07886ac0c192acd9e66841f26484d54a7bc809
KSZ08M <KSZ08M@cyber-dojo.org>
2023-01-28 15:12:01 +0000

    30 ran tests, no prediction, got amber

--- files/Hiker.kt
+++ files/Hiker.kt
@@ -4,12 +4,22 @@ import java.util.Arrays
 
 fun answer():Int {
     val initialState = Array(100){true}
+    
     for (i in 1..100){
-        initialState.forEachIndexed{ index , i ->
-        if(index % 2 == 1){
-            initialState[index] = !initialState[index]
+        if(i = 1){
+            initialState.forEachIndexed{ index , i ->
+                                        if(index % 2 == 0){
+                                            initialState[index] = !initialState[index]
+                                        }
+                                       }
         }
-    }
+        if(i = 2){
+            initialState.forEachIndexed{ index , i ->
+                                        if(index % 2 == 1){
+                                            initialState[index] = !initialState[index]
+                                        }
+                                       }
+        }
     }
     
     
