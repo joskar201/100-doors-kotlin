30ad5e58d9cb3b062d258467e53c0da633bcc60f
KSZ08M <KSZ08M@cyber-dojo.org>
2023-01-28 15:02:39 +0000

    25 ran tests, no prediction, got amber

--- files/Hiker.kt
+++ files/Hiker.kt
@@ -3,7 +3,7 @@ package hiker
 import java.util.Arrays
 
 fun answer():Int {
-    val initialState = Array(Boolean){true}
+    val initialState = BooleanArray(Boolean){true}
     initialState.forEachIndexed{ index , i ->
         if(index % 2 == 1){
             initialState[index] = !initialState[index]
